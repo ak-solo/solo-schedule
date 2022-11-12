6906756f2ded90de66ffec9e0f2912d571877cc4
ak-solo <ak.solo.2022@gmail.com>
2022-11-12 19:26:34 +0900

    WebAPIサービスにDbContext追加

--- src/SoloSchedule/SoloSchedule.WebAPI/Program.cs
+++ src/SoloSchedule/SoloSchedule.WebAPI/Program.cs
@@ -1,4 +1,17 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SoloSchedule.Infrastructure.SqlServer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+var cnnBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("SoloSchedule"))
+{
+    Password = builder.Configuration["DbPassword"],
+};
+var connection = cnnBuilder.ConnectionString;
+builder.Services.AddDbContext<SoloScheduleContext>(options => options.UseSqlServer(connection));
 
 // Add services to the container.
 builder.Services.AddControllers();
