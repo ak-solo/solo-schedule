fa1569c17b6c7ab356dbce95da44eeb3a2e96096
ak-solo <ak.solo.2022@gmail.com>
2022-11-12 11:08:02 +0900

    DBコンテキスト追加

---
+++ src/SoloSchedule/SoloSchedule.Infrastructure/SqlServer/SoloScheduleContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SoloSchedule.Infrastructure.SqlServer
+{
+    /// <summary>
+    /// SoloSchedule コンテキスト
+    /// </summary>
+    public class SoloScheduleContext : DbContext
+    {
+    }
+}
